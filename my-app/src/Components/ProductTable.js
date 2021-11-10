92273a5cc00c6e11aead5ce2ad07f4b6ade18b15
include-jacopo <jacopopati@gmail.com>
2021-11-10 12:56:27 +0100

    nuova schermata

--- my-app/src/Components/ProductTable.js
+++ my-app/src/Components/ProductTable.js
@@ -1,29 +1,29 @@
 import { Container, Row, Col, Table, ButtonGroup, ToggleButton } from 'react-bootstrap';
-import {PersonFill, GeoAltFill} from 'react-bootstrap-icons';
-import { Image } from 'react-bootstrap';
-import {useState} from 'react';
+import { PersonFill, GeoAltFill } from 'react-bootstrap-icons';
+import { Image, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { useState } from 'react';
 import "./ProductTable.css";
 
 
 
 
 function ProductTable(props) {
-// Here I create an array that contains all the product ids and the number of ordered products. I initialized it to zero.
-    let prodNum =[];
-    for (let i = 0 ; i < props.productByFarmer.length ; i++ ){
-        prodNum.push({"number" : 0, "pID" : props.productByFarmer[i].ProductID})
+    // Here I create an array that contains all the product ids and the number of ordered products. I initialized it to zero.
+    let prodNum = [];
+    for (let i = 0; i < props.productByFarmer.length; i++) {
+        prodNum.push({ "number": 0, "pID": props.productByFarmer[i].ProductID })
     }
     console.log(prodNum);
 
-//this function updates the number in the array, also allows to display the current number in the counter
-    function updateNumber(i, sign){
+    //this function updates the number in the array, also allows to display the current number in the counter
+    function updateNumber(i, sign) {
         if ((sign === -1 && prodNum[i].number !== 0) || (sign === +1 && prodNum[i].number < props.productByFarmer[i].Quantity)) {
-            prodNum[i].number+=sign;
+            prodNum[i].number += sign;
         }
         return prodNum[i].number;
     }
 
-   
+
 
     return (
         <Col>
@@ -41,83 +41,111 @@ function ProductTable(props) {
 
 function FarmerRow(props) {
     console.log("quantirendeding");
+
+    let product = [];
+
+    const splitEvery = (array, length) =>
+        array.reduce(
+            (result, item, index) => {
+                if (index % length === 0) result.push([])
+                result[Math.floor(index / length)].push(item)
+                return result
+            },
+            []
+        );
+
     return (<>
         <tr>
             <td className="producttable-col">
-                <h1 style={{ fontSize: 25 }} align={"left"}>{props.farmer.Company}</h1>
 
-                <section className = "d-flex justify-content-between">
-                    <div> <PersonFill/><span>&nbsp;</span>
-                    {props.farmer.Name}<span>&nbsp;</span>{props.farmer.Surname}
-                    </div>
-                    <div>
-                    <GeoAltFill className="ml-3"/><span>&nbsp;</span>
-                    {props.farmer.Address}<span>,&nbsp;</span>{props.farmer.State}
-                    </div>
-                </section>
+                <Container>
+                    <Row>
+                        <h1 style={{ fontSize: 25 }} align={"left"}>{props.farmer.Company}</h1>
+                    </Row>
+                    <Row>
+                        <section className="d-flex justify-content-between">
+                            <div> <PersonFill /><span>&nbsp;</span>
+                                {props.farmer.Name}<span>&nbsp;</span>{props.farmer.Surname}
+                            </div>
+                            <div>
+                                <GeoAltFill className="ml-3" /><span>&nbsp;</span>
+                                {props.farmer.Address}<span>,&nbsp;</span>{props.farmer.State}
+                            </div>
+                        </section>
+                    </Row>
 
-                <Table striped hover size="sm" className="justify-content-center">
-                    <thead>
-                        <tr>
-                            <th>Image</th>
-                            <th>Product Name</th>
-                            <th>Description</th>
-                            <th>Quantity</th>
-                            <th>Unit</th>
-                            <th>Cost</th>
-                            <td></td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.productByFarmer.map((p, idx) =>
-                            p.FarmerID === props.farmer.FarmerID ?
-                                <tr className="centrami">
-                                    <td><Image src="/images/placeholder.jpg" rounded width={"60px"} /></td>
-                                    <td>{p.NameProduct}</td>
-                                    <td>{p.Description}</td>
-                                    <td>{p.Quantity}</td>
-                                    <td>{p.UnitOfMeasurement}</td>
-                                    <td>{p.Price}€</td>
-                                    <td><ProductsCounter numIndex={idx} updateNumber={props.updateNumber}/></td>
-                                </tr>
-                                : '')}
+                    {props.productByFarmer.map(p =>
+                        p.FarmerID === props.farmer.FarmerID ? product.push(p) : ''
+                    )}
 
-                    </tbody>
-                </Table>
+                    {splitEvery(product, 3).map(p => (
+                        <Row className = "mb-4">
+                            {p.map(pf => (
+                                <Col md>
+                                    <ProductCard></ProductCard>
+                                </Col>
+                            ))}
+                        </Row>
+                    ))}
+
+                </Container>
             </td>
         </tr>
     </>
     );
 };
 
+function ProductCard(props) {
+    return (
+
+        <Card style={{ width: '11rem' }}>
+            <Card.Img variant="top" src="/images/placeholder.jpg" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+        </Card>
+    );
+}
 
 
-
-function ProductsCounter(props){
+function ProductsCounter(props) {
     const [number, setNumber] = useState(0)
 
-    function updateIndex(sign){
-       let i = props.updateNumber(props.numIndex,sign);
-       setNumber (i);
-       console.log(i);
+    function updateIndex(sign) {
+        let i = props.updateNumber(props.numIndex, sign);
+        setNumber(i);
+        console.log(i);
 
     }
     return (
-     <ButtonGroup>
+        <ButtonGroup>
 
-      <ToggleButton style={{ minWidth : "2.5rem"}} variant='secondary' onClick={() => updateIndex(-1)}>{/*
+            <ToggleButton style={{ minWidth: "2.5rem" }} variant='secondary' onClick={() => updateIndex(-1)}>{/*
 
  */}
- -
-      </ToggleButton>
-      <ToggleButton style={{ minWidth : "3rem"}} disabled variant="light">
-          {number}
-      </ToggleButton>
-        <ToggleButton style={{ minWidth : "2.5rem"}} variant="secondary" onClick={() => updateIndex(+1)} >
-+
-      </ToggleButton>
+                -
+            </ToggleButton>
+            <ToggleButton style={{ minWidth: "3rem" }} disabled variant="light">
+                {number}
+            </ToggleButton>
+            <ToggleButton style={{ minWidth: "2.5rem" }} variant="secondary" onClick={() => updateIndex(+1)} >
+                +
+            </ToggleButton>
 
-  </ButtonGroup>
+        </ButtonGroup>
 
     );
 
