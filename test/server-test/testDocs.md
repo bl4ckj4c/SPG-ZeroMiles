385dbc183119fc09d73c979f11455b3ca5136886
bl4ckj4c <jacopo.marino.1998@gmail.com>
2021-11-10 15:45:59 +0100

    Unit tests for GET all users and for POST new user, with documentation

--- test/server-test/testDocs.md
+++ test/server-test/testDocs.md
@@ -8,16 +8,16 @@ The unit tests for the back-end side are written using Python as language. This 
 
 The unit test of this method consists in sending the `GET` request to the server and check if the response is correct and the JSON is well-formed. The URL contacted is `/api/farmers`. The test method uses two arrays, one is for store the fields that need to be checked and the other contains the expected formatting in the form of regular expressions.<br/>The outer for loop checks every farmer retrieved by the server, while the inner loop checks that each object inside the array is well-formed, so all fields present and valid.
 
-| Field    | Regex                                               |
-| -------- | --------------------------------------------------- |
-| Name     | `^[a-zA-Z]+$`                                       |
-| Surname  | `^[a-zA-Z]+$`                                       |
-| FarmerID | `^.+$`                                              |
-| Email    | `^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$`  |
-| Phoneno  | `^(\+(\([0-9]{1,2}\))?)?[0-9]+$`                    |
-| Address  | `^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$`                    |
-| State    | `^[a-zA-Z]+$`                                       |
-| Zipcode  | `^[0-9]{5}`                                         |
+| Field    | Regex                                              |
+| -------- | -------------------------------------------------- |
+| Name     | `^[a-zA-Z]+$`                                      |
+| Surname  | `^[a-zA-Z]+$`                                      |
+| FarmerID | `^.+$`                                             |
+| Email    | `^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$` |
+| Phoneno  | `^(\+(\([0-9]{1,2}\))?)?[0-9]+$`                   |
+| Address  | `^(via|Via|corso|Corso|piazza|Piazza)\s[a-zA-Z\s\']+(\s+|\,\s*)([1-9][0-9]*)$`                                            |
+| State    | `^[a-zA-Z]+$`                                      |
+| Zipcode  | `^[0-9]{5}`                                        |
 
 The output of the test is none if it passes, otherwise it's a JSON object containing an array in which object contains the wrong farmer and the wrong field(s) that caused the fail of the test.
 
@@ -53,24 +53,24 @@ The output of the test is none if it passes, otherwise it's a JSON object contai
 
 The unit test of this method consists in sending the `GET` request to the server and check if the response is correct and the JSON is well-formed. The URL contacted is `/api/productByFarmer`. The test method uses two arrays, one is for store the fields that need to be checked and the other contains the expected formatting in the form of regular expressions.<br/>The outer for loop checks every product by farmer retrieved by the server, while the inner loop checks that each object inside the array is well-formed, so all fields present and valid.
 
-| Field             | Regex                                               |
-| ----------------- | --------------------------------------------------- |
-| FarmerID          | `^.+$`                                              |
-| Name              | `^[a-zA-Z]+$`                                       |
-| Surname           | `^[a-zA-Z]+$`                                       |
-| Company           | `^[a-zA-Z\-\_\s0-9\.\,]+$`                          |
-| Email             | `^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$`  |
-| Phoneno           | `^(\+(\([0-9]{1,2}\))?)?[0-9]+$`                    |
-| Address           | `^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$`                    |
-| State             | `^[a-zA-Z]+$`                                       |
-| Zipcode           | `^[0-9]{5}$`                                        |
-| ProductID         | `^.+$`                                              |
-| NameProduct       | `^[a-zA-Z\-\_0-9\.\,]+$`                            |
-| Description       | `^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$`                    |
-| ImageID           | `^[0-9]+$`                                          |
-| Quantity          | `^[0-9]+$`                                          |
-| UnitOfMeasurement | `^[a-zA-Z]+$`                                       |
-| Price             | `^([1-9][0-9]{,2}(,[0-9]{3})*`                      |
+| Field             | Regex                                                        |
+| ----------------- | ------------------------------------------------------------ |
+| FarmerID          | `^.+$`                                                       |
+| Name              | `^[a-zA-Z]+$`                                                |
+| Surname           | `^[a-zA-Z]+$`                                                |
+| Company           | `^[a-zA-Z\-\_\s0-9\.\,]+$`                                   |
+| Email             | `^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$`           |
+| Phoneno           | `^(\+(\([0-9]{1,2}\))?)?[0-9]+$`                             |
+| Address           | `^(via|Via|corso|Corso|piazza|Piazza)\s[a-zA-Z\s\']+(\s+|\,\s*)([1-9][0-9]*)$` |
+| State             | `^[a-zA-Z]+$`                                                |
+| Zipcode           | `^[0-9]{5}$`                                                 |
+| ProductID         | `^.+$`                                                       |
+| NameProduct       | `^[a-zA-Z\-\_0-9\.\,]+$`                                     |
+| Description       | `^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$`                             |
+| ImageID           | `^[0-9]+$`                                                   |
+| Quantity          | `^[0-9]+$`                                                   |
+| UnitOfMeasurement | `^[a-zA-Z]+$`                                                |
+| Price             | `^([1-9][0-9]{,2}(,[0-9]{3})*`                               |
 
 The output of the test is none if it passes, otherwise it's a JSON object containing an array in which object contains the wrong farmer and the wrong field(s) that caused the fail of the test.
 
@@ -127,4 +127,6 @@ The body of the `POST` request is the following:
 ```
 
 If the insertion worked correctly, then the test checks if the new user is actually present into Firebase. If yes, the test is passed and the test user is removed from the database.
+
+For the wrong user case, the test checks if the user has been inserted in Firebase: if yes, that means that the validations done by server are not working properly, so the new user is removed and the test is failed.
 
--- test/server-test/unitTest.py
+++ test/server-test/unitTest.py
@@ -1,12 +1,15 @@
+# Library for HTTP RESTful requests
 import requests
+# Library for unit test management
 import unittest
+# LIbrary for JSON parsing
 import json
+# Library for regexp
 import re
 
-# Firebase management
+# Firebase management libraries
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate(
     "./polito-se2-21-01-spg-firebase-adminsdk-76fui-7b28269ea6.json")
@@ -18,13 +21,99 @@ db = firestore.client()
 
 def TestSuiteBackend():
     suite = unittest.TestSuite()
+
+    suite.addTest(TestStorySPG2('test_GET_users'))
     suite.addTest(TestStorySPG2('test_POST_correct_user'))
+    suite.addTest(TestStorySPG2('test_POST_wrong_user'))
     suite.addTest(TestStorySPG3('test_GET_farmers'))
     suite.addTest(TestStorySPG3('test_GET_productsByFarmer'))
     return suite
 
 
 class TestStorySPG2(unittest.TestCase):
+
+    def test_GET_users(self):
+        errorLog = {
+            'errors': []
+        }
+        flagError = False
+
+        # Request sent
+        r = requests.get('http://localhost:3001/api/users')
+
+        # Check the response status code
+        self.assertEqual(r.status_code, 200)
+
+        # Check if the JSON is well-formed
+        try:
+            JSONbody = r.json()
+
+            # Arrays for tests
+            fields = [
+                'Name',
+                'Surname',
+                'UserID',
+                'Email',
+                'Phoneno',
+                'Address',
+                'State',
+                'Zipcode'
+            ]
+            regexes = [
+                r'^[a-zA-Z]+$',
+                r'^[a-zA-Z]+$',
+                r'^.+$',
+                r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$',
+                r'^(\+(\([0-9]{1,2}\))?)?[0-9]+$',
+                r'^(via|Via|corso|Corso|piazza|Piazza)\s[a-zA-Z\s\']+(\s+|\,\s*)([1-9][0-9]*)$',
+                r'^[a-zA-Z]+$',
+                r'^[0-9]{5}$'
+            ]
+
+            # Check that the JSON has all the fields expected and that them are all well-formed
+            for user in JSONbody:
+                tmpError = {
+                    'user': user,
+                    'errors': []
+                }
+
+                for item, regex in zip(fields, regexes):
+                    if item not in user:
+                        tmpError['errors'].append({
+                            'field': item,
+                            'error': 'missing'
+                        })
+                        flagError = True
+                    else:
+                        # Check that item is a string
+                        if not isinstance(user[item], str):
+                            tmpError['errors'].append({
+                                'field': item,
+                                'error': 'wrong format, not a string'
+                            })
+                            flagError = True
+                        # Check that item is a correct string
+                        pattern = re.compile(regex)
+
+                        #print(str(item) + ' -> regex: ' + str(pattern.match(user[item]).group(0)))
+
+                        if not pattern.match(user[item]):
+                            tmpError['errors'].append({
+                                'field': item,
+                                'error': 'wrong format, not matching the regex'
+                            })
+                            flagError = True
+
+                if len(tmpError['errors']) > 0:
+                    errorLog['errors'].append(tmpError)
+
+            # There is at least one error
+            if flagError:
+                self.fail(json.dumps(errorLog, indent=4))
+
+        # The body in the response is not a valid JSON
+        except requests.exceptions.JSONDecodeError:
+            self.fail('The body received is not a valid JSON')
 
     def test_POST_correct_user(self):
         user = {
@@ -54,12 +143,47 @@ class TestStorySPG2(unittest.TestCase):
         flagUser = False
             
         for doc in docs:
-            print(f'Remove => {json.dumps(doc.to_dict(), indent=4)}')
+            # print(f'Remove => {json.dumps(doc.to_dict(), indent=4)}')
             db.collection(u'User').document(doc.id).delete()
             flagUser = True          
             
         # otherwise prompt an error, beacuse the server didn't work properly
         self.assertTrue(flagUser, 'The server didn\'t work properly, the test user is not present in Firebase.')
+
+    def test_POST_wrong_user(self):
+        user = {
+            'name': '123test',
+            'lastName': 'TEST456',
+            'email': 'test.test@gmail.',
+            'address': 'Via Test ',
+            'phone': 'mywrongphonenumber',
+            'city': 'mywrongcity234',
+            'password': ''
+        }
+
+        # Request sent
+        r = requests.post(
+           'http://localhost:3001/api/register',
+           json=user
+        )
+
+        # Check the response status code
+        self.assertEqual(r.status_code, 400)
+
+        # If the insertion was successful, then remove the test user form Firebase
+        # Check if the new user is present in Firebase
+        # If yes, then remove the new user
+        users_ref = db.collection(u'User').where(u'name', u'==', u'123test')
+        docs = users_ref.stream()
+        flagUser = False
+            
+        for doc in docs:
+            # print(f'Remove => {json.dumps(doc.to_dict(), indent=4)}')
+            db.collection(u'User').document(doc.id).delete()
+            flagUser = True          
+            
+        # otherwise prompt an error, beacuse the server didn't work properly
+        self.assertFalse(flagUser, 'The server validation didn\'t work properly, the test user was present in Firebase.')
 
 
 class TestStorySPG3(unittest.TestCase):
@@ -84,6 +208,7 @@ class TestStorySPG3(unittest.TestCase):
             fields = [
                 'Name',
                 'Surname',
+                'Company',
                 'FarmerID',
                 'Email',
                 'Phoneno',
@@ -94,10 +219,11 @@ class TestStorySPG3(unittest.TestCase):
             regexes = [
                 r'^[a-zA-Z]+$',
                 r'^[a-zA-Z]+$',
+                r'^[a-zA-Z\-\_\s0-9\.\,]+$',
                 r'^.+$',
                 r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$',
                 r'^(\+(\([0-9]{1,2}\))?)?[0-9]+$',
-                r'^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$',
+                r'^(via|Via|corso|Corso|piazza|Piazza)\s[a-zA-Z\s\']+(\s+|\,\s*)([1-9][0-9]*)$',
                 r'^[a-zA-Z]+$',
                 r'^[0-9]{5}$'
             ]
@@ -186,19 +312,22 @@ class TestStorySPG3(unittest.TestCase):
                 'Price'
             ]
             regexes = [
+                # Farmer
                 r'^.+$',
                 r'^[a-zA-Z]+$',
                 r'^[a-zA-Z]+$',
                 r'^[a-zA-Z\-\_\s0-9\.\,]+$',
                 r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$',
                 r'^(\+(\([0-9]{1,2}\))?)?[0-9]+$',
-                r'^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$',
+                r'^(via|Via|corso|Corso|piazza|Piazza)\s[a-zA-Z\s\']+(\s+|\,\s*)([1-9][0-9]*)$',
                 r'^[a-zA-Z]+$',
                 r'^[0-9]{5}$',
+                # Product
                 r'^.+$',
                 r'^[a-zA-Z\-\_0-9\.\,]+$',
                 r'^[a-zA-Z\,\.0-9\t\n\r\f\v\s]+$',
                 r'^[0-9]+$',
+                # Product by farmer
                 r'^[0-9]+$',
                 r'^[a-zA-Z]+$',
                 r'^([1-9][0-9]{,2}(,[0-9]{3})*|[0-9]+)(\.[0-9]{1,9})?$',
