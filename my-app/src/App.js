8684a9cc69ae44e5126aeb16611bd757c16e2e07
include-jacopo <jacopopati@gmail.com>
2021-11-12 10:34:17 +0100

    toast disabilitato

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -50,21 +50,21 @@ function App() {
   //Gestione di eventuali errori in risposta alle API
   const handleErrors = (err) => {
     {/*setMessage({ msg: err.error, type: 'danger' });*/ }
-    setMessage({ msg: "Dear customer, we are experiencing some technical difficulties. Please come back later.", type: 'danger' });
+    //setMessage({ msg: "Dear customer, we are experiencing some technical difficulties. Please come back later.", type: 'danger' });
     console.log(err);
   }
 
   return (
     <Router>
       {/* Visualizzazione di eventuali errori gestiti dalla funzione handleErrors*/}
-      <ToastContainer className="p-3" position="middle-center">
+      {/*ToastContainer className="p-3" position="middle-center">
         <Toast bg="warning" onClose={() => setMessage('')} delay={3000} autohide>
           <Toast.Header closeButton={false}>
             <strong className="me-auto">Error :(</strong>
           </Toast.Header>
           <Toast.Body>{message?.msg}</Toast.Body>
         </Toast>
-      </ToastContainer>
+        </ToastContainer> */}
 
       <Navbar bg="warning">
         <Container>
