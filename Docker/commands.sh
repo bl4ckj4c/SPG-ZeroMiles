ff5a4adbad460d6d3d12d3eb459f6686f6fd0a37
NOED <bruno.accogli@libero.it>
2021-11-15 04:09:25 -0800

    Docker configuration files

---
+++ Docker/commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "git clone https://github.com/bl4ckj4c/polito-se2-21-P01-SPG"
+git clone https://github.com/bl4ckj4c/polito-se2-21-P01-SPG
+
+cd polito-se2-21-P01-SPG
+
+echo "git checkout origin/master"
+git checkout origin/master
+
+#echo "installing server modules..."
+#cd Server/
+#npm i
+
+#echo "installing my-app modules..."
+#cd ../my-app
+#npm i
+
+#echo "starting server & client"
+#npm start & (cd ../Server && node server.js)
+
