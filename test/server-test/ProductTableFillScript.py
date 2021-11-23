2b8b57418b0c48ecc8d45e5b04aaef55997f01d8
bl4ckj4c <jacopo.marino.1998@gmail.com>
2021-11-23 17:01:29 +0100

    Update ProductTableFillScript.py

--- test/server-test/ProductTableFillScript.py
+++ test/server-test/ProductTableFillScript.py
@@ -4,6 +4,8 @@ import json
 import random
 import numpy as np
 
+import time
+
 # Firebase management libraries
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -22,9 +24,9 @@ def loadProductsToFirebase():
 
     count = 0
 
-    #for product in JSONProducts['products']:
+    # for product in JSONProducts['products']:
     for i in range(1, 5):
-        #print(product)
+        # print(product)
         product = JSONProducts['products'][i]
 
         data = {
@@ -38,12 +40,14 @@ def loadProductsToFirebase():
 
         print(count)
 
+
 def deleteProducts():
     # Delete tuples in Product by Farmers
     docs = db.collection(u'Product').get()
     for doc in docs:
         print(f'Remove tuple => {json.dumps(doc.to_dict(), indent=4)}\n')
         db.collection(u'Product').document(doc.id).delete()
+
 
 def deleteProductByFarmersTuples():
     # Delete tuples in Product by Farmers
@@ -52,12 +56,13 @@ def deleteProductByFarmersTuples():
         #print(f'Remove tuple => {json.dumps(doc.to_dict(), indent=4)}\n')
         db.collection(u'Product by Farmers').document(doc.id).delete()
 
+
 def insertProductByFarmersTuples():
     # Insertion of Product by Farmers
     Farmers = db.collection(u'Farmer').get()
     Products = db.collection(u'Product').get()
 
-    quantities= range(1, 10)
+    quantities = range(1, 10)
     units = ['kg', 'bag']
     prices = np.arange(0.5, 30, 0.5)
 
@@ -76,7 +81,6 @@ def insertProductByFarmersTuples():
             else:
                 price = int(random.choice(prices))
 
-
             data = {
                 'FarmerID': farmer.id,
                 'ProductID': randomProduct.id,
@@ -87,8 +91,64 @@ def insertProductByFarmersTuples():
             #print(json.dumps(data, indent=4))
             db.collection(u'Product by Farmers').add(data)
 
+def deleteOrdersTuples():
+    # Delete tuples in Order
+    docs = db.collection(u'Order').get()
+    for doc in docs:
+        #print(f'Remove tuple => {json.dumps(doc.to_dict(), indent=4)}\n')
+        db.collection(u'Order').document(doc.id).delete()
+
+def insertOrdersTuples():
+    Clients = db.collection(u'User').get()
+    Products = db.collection(u'Product').get()
+    ProductByFarmers = db.collection(u'Product by Farmers').get()
+
+    quantities = range(1, 10)
+
+    for client in Clients:
+        clientDict = client.to_dict()
+        productList = []
+
+        numbers = range(1, random.choice([2, 3, 4, 5]))
+
+        for i in numbers:
+
+            randomProduct = random.choice(Products)
+            randomProductDict = randomProduct.to_dict()
+
+            productByFarmer = db.collection(u'Product by Farmers').where('ProductID', '==', str(randomProduct.id)).get()
+            productByFarmerDict = productByFarmer[0].to_dict()
+
+            #print(json.dumps(randomProductDict, indent=4))
+            #print(json.dumps(productByFarmer[0].to_dict(), indent=4))
+
+
+            quantity = random.choice(quantities)
+
+            productList.append({
+                'FarmerID': productByFarmerDict['FarmerID'],
+                'ImageID': randomProductDict['ImageID'],
+                'NameProduct': randomProductDict['Name'],
+                'Price': productByFarmerDict['Price'],
+                'ProductID': productByFarmerDict['ProductID'],
+                'number': quantity
+            })
+
+        data = {
+            'ClientID': client.id,
+            'Products': productList,
+            'Status': 'open',
+            'Timestamp': time.strftime('%d-%m-%Y %H:%M:%S', time.localtime())
+        }
+        #print(json.dumps(data, indent=4))
+        db.collection(u'Order').add(data)
+
+
 if __name__ == '__main__':
-    #deleteProducts()
-    #loadProductsToFirebase()
-    deleteProductByFarmersTuples()
-    insertProductByFarmersTuples()
+    # deleteProducts()
+    # loadProductsToFirebase()
+    # deleteProductByFarmersTuples()
+    # insertProductByFarmersTuples()
+    deleteOrdersTuples()
+    insertOrdersTuples()
+    print('DONE')
