19d68ae81f1d99f759ced83937a79e09c418a4a8
include-jacopo <jacopopati@gmail.com>
2021-11-23 17:08:32 +0100

    fixed the column displayed

--- my-app/src/Components/ProductTable.js
+++ my-app/src/Components/ProductTable.js
@@ -158,7 +158,7 @@ function CartBottomButton(props) {
         <Button variant={total > props.Wallet ? "danger" : "secondary"} className="fixed-right-bottom" onClick={props.handleCartCheckoutModalShow}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
         </svg>
-        €{total.toFixed(2)} </Button>
+            €{total.toFixed(2)} </Button>
     );
 }
 
@@ -266,7 +266,12 @@ function FarmerRow(props) {
                                     <Col key={pf.ProductID + "_" + pf.FarmerID} xl className="column-margin">
                                         <ProductCard isLoggedIn={props.isLoggedIn} unfilteredProductByFarmer={props.unfilteredProductByFarmer} prodNum={props.prodNum} productByFarmer={props.productByFarmer} prodottoDelFarmer={pf} UpdateNumber={props.UpdateNumber} />
                                     </Col>
-                                ))}
+                                )
+                                )
+                                }
+                                {console.log("ciaooooo" + p.size)}
+                                {p.length === 2 ? <> <Col> </Col> </> : ''}
+                                {p.length === 1 ? <> <Col> </Col> <Col> </Col> </> : ''}
                             </Row>
                         ))}
 
