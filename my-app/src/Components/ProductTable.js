dc4feaece3806bdd6530bd95ba077fbc759a0268
include-jacopo <jacopopati@gmail.com>
2021-11-24 16:11:16 +0100

    moved cart to left for mobile

--- my-app/src/Components/ProductTable.js
+++ my-app/src/Components/ProductTable.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col, Table, ButtonGroup, ToggleButton } from 'react-bootstrap';
-import { PersonFill, GeoAltFill, TypeH1, Collection, Bag, Cash, CartCheckFill, AlignMiddle } from 'react-bootstrap-icons';
+import { PersonFill, GeoAltFill, TypeH1, Collection, Bag, Cash, CartCheckFill, Cart4 } from 'react-bootstrap-icons';
 import { Image, Card, ListGroup, ListGroupItem, Form, Button, Collapse, Modal } from 'react-bootstrap';
 import { useState } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
@@ -191,9 +191,8 @@ function CartBottomButton(props) {
     props.prodNum.forEach(p => p.number > 0 ? total += p.number * p.Price : "")
     if (wallet!==false)
         return (
-        <Button variant={total > wallet ? "danger" : "secondary"} className="fixed-right-bottom" onClick={props.handleCartCheckoutModalShow}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
-            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-        </svg>
+        <Button variant={total > wallet ? "danger" : "secondary"} className="fixed-button-bottom" onClick={props.handleCartCheckoutModalShow}>
+            <Cart4 size={25}/>
         <span>&nbsp;</span> €{total.toFixed(2)} </Button>
      );
      else
