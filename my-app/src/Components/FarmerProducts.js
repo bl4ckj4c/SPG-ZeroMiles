493ec3ab3dbe6c6bddd0da8e100c07f1919067a5
taurou <80039469+taurou@users.noreply.github.com>
2021-12-09 21:27:32 +0100

    progress on FarmerProduct.js

--- my-app/src/Components/FarmerProducts.js
+++ my-app/src/Components/FarmerProducts.js
@@ -1,4 +1,4 @@
-import { Form, Card, InputGroup, Button, Col, Container, Modal,Row } from 'react-bootstrap';
+import { Form, Card,ListGroup,  Table, InputGroup, Button, Col, Container, Modal,Row } from 'react-bootstrap';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import API from '../API';
 import { useState, useEffect } from 'react'
@@ -71,25 +71,69 @@ function FarmerProducts(props) {
         , [ProductsByFarmerUpdate]);
 
 
-    if (updated) return (
-        <Container className= "search-container"  >
-        <Row><Col className="poss"><Button >Add Product for Next Week</Button></Col>
-        <Col  lg={9} >
+  if (updated) return (
+    <Container className="search-container"  >
 
-    <ProductsDropdown products={productsByFarmer.length >0 ? products.filter(pp => !productsByFarmer.some(pbf => pbf.ProductID === pp.ProductID)) : products  } setSelectedProduct={setSelectedProduct} selectedProduct={selectedProduct} />
-    <Button className="search-button"  disabled={selectedProduct.length > 0 ? false : true } onClick={() => setAddProdShow(true)}>Add product</Button>
-            {productsByFarmer!==false ? productsByFarmer.map( p=>  <div><ProductCard key={p.ProdByFarmerID} p={p}  deleteProductByFarmer={deleteProductByFarmer} addProdTest={addProdTest}/></div> ) : "" } 
-<AddProductModal addProdTest={addProdTest}  product={selectedProduct.length > 0 ? selectedProduct[0] : null } show={addProdShow} onHide={() => setAddProdShow(false)}/>
-</Col></Row>
-    </Container>)
+          <ProductsDropdown products={productsByFarmer.length > 0 ? products.filter(pp => !productsByFarmer.some(pbf => pbf.ProductID === pp.ProductID)) : products} setSelectedProduct={setSelectedProduct} selectedProduct={selectedProduct} />
+          <Button className="search-button" disabled={selectedProduct.length > 0 ? false : true} onClick={() => setAddProdShow(true)}>Add product</Button>
+
+          <Table className="d-flex justify-content-center">
+          <tbody id="prod-table" align="center">
+            {productsByFarmer !== false ? productsByFarmer.map(p => 
+              <ProductCard key={p.ProdByFarmerID} p={p} deleteProductByFarmer={deleteProductByFarmer} addProdTest={addProdTest} />
+           ) : ""}
+            </tbody>
+          </Table>
+        <AddProductModal addProdTest={addProdTest} product={selectedProduct.length > 0 ? selectedProduct[0] : null} show={addProdShow} onHide={() => setAddProdShow(false)} />
+      
+    </Container >)
     else return "";
 
 }
 
 
+function ProductCard(props) {
+  const [show, setShow] = useState(false);
 
 
-function ProductCard(props){
+  return (
+      <>
+          <EditProductModal addProdTest={props.addProdTest} p={props.p} show={show} onHide={() => setShow(false)}/>
+
+      <Card className="client-card mt-3">
+
+          <Card.Body>
+              <Row>
+                  <Col md={4}>
+{/*                      <PersonCircle size={60} style={{ marginBottom: '6px', marginRight: '5px' }} />
+ */}                  </Col>
+                  <Col md={8}>
+                      <Card.Title style={{ fontSize: 28 }}> {props.p.NameProduct}</Card.Title>
+                  </Col>
+              </Row>
+          </Card.Body>
+
+
+          <Card.Body>
+              <ListGroup style={{ textAlign: "left" }}>
+                  <ListGroup.Item> Quantity: {props.p.Quantity}</ListGroup.Item>
+                  <ListGroup.Item> Unit: {props.p.UnitOfMeasurement}</ListGroup.Item>
+                  <ListGroup.Item> Price: {props.p.Price}</ListGroup.Item>
+              </ListGroup>
+          </Card.Body>
+
+          <Card.Body className="sfondo-footer" style={{ textAlign: "right" }}>
+          <Button style={{ "marginRight" : "0.3rem"}}  variant="outline-dark" size="sm" onClick={() => setShow(true)}>Edit</Button>
+          <Button variant="danger" size="sm" onClick={() => props.deleteProductByFarmer(props.p.ProdByFarmerID)}>Delete</Button>
+          </Card.Body>
+      </Card>
+      </>
+  );
+}
+
+
+
+function ProductCard2(props){
     const [show, setShow] = useState(false);
 
 
@@ -102,7 +146,7 @@ function ProductCard(props){
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
             <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
-            <svg onClick={() => props.deleteProductByFarmer(props.p.ProdByFarmerID)} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-trash createSurUpDownDeleteMargin" viewBox="0 0 16 16">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-trash createSurUpDownDeleteMargin" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
               <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
             </svg>
