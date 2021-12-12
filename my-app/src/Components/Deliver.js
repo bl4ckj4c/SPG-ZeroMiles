c2610e760efe15eb4fc9776dcb4a3e74c1449b62
include-jacopo <jacopopati@gmail.com>
2021-12-12 19:12:32 +0100

    fixed time picker for delivery

--- my-app/src/Components/Deliver.js
+++ my-app/src/Components/Deliver.js
@@ -1,11 +1,11 @@
 import API from '../API';
-import {useState, useEffect, useCallback} from 'react';
-import {Table, Row, Col, Form, ListGroup, Spinner, Card, Modal,Image, Button, Container} from 'react-bootstrap';
-import {PersonCircle, GeoAltFill, MapFill, WalletFill,PersonFill, ClockFill} from 'react-bootstrap-icons';
+import { useState, useEffect, useCallback } from 'react';
+import { Table, Row, Col, Form, ListGroup, Spinner, Card, Modal, Image, Button, Container } from 'react-bootstrap';
+import { PersonCircle, GeoAltFill, MapFill, WalletFill, PersonFill, ClockFill } from 'react-bootstrap-icons';
 import "./ClientView.css";
 import { useLocation } from 'react-router-dom';
 import "./ClientOrders.css";
-import {useDropzone} from 'react-dropzone'
+import { useDropzone } from 'react-dropzone'
 //import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import TimePicker from 'react-time-picker';
@@ -20,19 +20,19 @@ function Deliver(props) {
     const [modalShowProductNew, setModalShowProductNew] = useState(false);
 
     async function submitDelivery() {
-               var datestring = dateDelivery.getMonth()+1  + "-" + (dateDelivery.getDate()) + "-" + dateDelivery.getFullYear()
-               datestring = datestring + " " +timeDelivery
-               datestring = datestring+":00" //Add the seconds to the datetime delivery
-                 let objectDelivery = {
-                    "OrderID": props.orderId,
-                    "DeliveryPlace": address,
-                    "DeliveryDate": datestring
-                }
-                let res = await API.modifyDelivery(objectDelivery);
-                console.log(objectDelivery);
-                console.log(res);
-                setModalShowProductNew(false)
-            }
+        var datestring = dateDelivery.getMonth() + 1 + "-" + (dateDelivery.getDate()) + "-" + dateDelivery.getFullYear()
+        datestring = datestring + " " + timeDelivery
+        datestring = datestring + ":00" //Add the seconds to the datetime delivery
+        let objectDelivery = {
+            "OrderID": props.orderId,
+            "DeliveryPlace": address,
+            "DeliveryDate": datestring
+        }
+        let res = await API.modifyDelivery(objectDelivery);
+        console.log(objectDelivery);
+        console.log(res);
+        setModalShowProductNew(false)
+    }
 
 
 
@@ -41,20 +41,20 @@ function Deliver(props) {
         <>
             {
                 <>
-                            <DeliveryBottom  onShow={() => setModalShowProductNew(true)}/>
-                             <ModalDeliveryNew
-                                        setAddress={setAddress}
-                                        setDateDelivery={setDateDelivery}
-                                        setTimeDelivery={setTimeDelivery}
-                                        submitDelivery={submitDelivery}
-                                        show={modalShowProductNew}
-                                        onHide={() => setModalShowProductNew(false)}/>
+                    <DeliveryBottom onShow={() => setModalShowProductNew(true)} />
+                    <ModalDeliveryNew
+                        setAddress={setAddress}
+                        setDateDelivery={setDateDelivery}
+                        setTimeDelivery={setTimeDelivery}
+                        submitDelivery={submitDelivery}
+                        show={modalShowProductNew}
+                        onHide={() => setModalShowProductNew(false)} />
 
-                    
+
 
                 </>
             }
-       </>
+        </>
     );
 }
 
@@ -65,30 +65,38 @@ function ModalDeliveryNew(props) {
 
     return (
         <Modal show={props.show} onHide={props.onHide} size="md" aria-labelledby="contained-modal-title-vcenter"
-               centered>
+            centered>
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                Schedule delivery
+                    Schedule delivery
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Form onSubmit={(e) => props.submitDelivery(e)} controlId="my-form">
-                    <Form.Group className="mb-3" controlId="address">
-                        <Form.Label className="label">Address</Form.Label>
-                        <Form.Control type="text" placeholder="Enter address"
-                                      onChange={(e) => props.setAddress(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="dateDelivery">
-                        <Form.Label className="label">Date for delivery</Form.Label>
-                        <DateDeliveryRow setDateDelivery={props.setDateDelivery} ></DateDeliveryRow>
-                        <TimeDeliveryRow setTimeDelivery={props.setTimeDelivery} ></TimeDeliveryRow>
-                   </Form.Group>
-                    <Button onClick={props.submitDelivery} variant="success">Create</Button>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="address">
+                                <Form.Label className="label">Address</Form.Label>
+                                <Form.Control type="text" placeholder="Enter address"
+                                    onChange={(e) => props.setAddress(e.target.value)} />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="dateDelivery">
+                                <Form.Label className="label">Date for delivery</Form.Label>
+                                <DateDeliveryRow setDateDelivery={props.setDateDelivery} ></DateDeliveryRow>
+                                <TimeDeliveryRow setTimeDelivery={props.setTimeDelivery} ></TimeDeliveryRow>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Button variant='secondary' onClick={props.onHide}>Close</Button>
                     {' '}{' '}
-                    <Button variant='danger' onClick={props.onHide}>Close</Button>
+                    <Button onClick={props.submitDelivery} variant="warning">Create</Button>
+                    
+                    
                 </Form>
             </Modal.Body>
-        </Modal>
+        </Modal >
     );
 }
 
@@ -103,28 +111,31 @@ function DeliveryBottom(props) {
 
 const DateDeliveryRow = (props) => {
     const [startDate, setStartDate] = useState(new Date());
-    return (                                   
-      <DatePicker selected={startDate}
-      format="dd-MM-yyyy" 
-       onChange={(date) => setStartDate(date)}
-       onChange={(date) => props.setDateDelivery(date)}
-       />
-    );
-  };
-
-  const TimeDeliveryRow = (props) => {
-    const [value, onChange] = useState('10:00:00');
-  
     return (
-      <div>
-        <TimePicker
-          onChange={onChange}
-          onChange={(value) =>props.setTimeDelivery(value)}
-          value={value}
+        <DatePicker
+            dateFormat="dd-MM-yyyy"
+            selected={startDate}
+            value={startDate}
+            onChange={(date) => { setStartDate(date); props.setDateDelivery(date); }}
         />
-      </div>
     );
-  };
+};
+
+const TimeDeliveryRow = (props) => {
+    const [value, onChange] = useState('10:00');
+
+    props.setTimeDelivery(value);
+
+    return (
+        <div>
+            <TimePicker
+                onChange={(value) => { onChange(value); props.setTimeDelivery(value); }}
+                value={value}
+                disableClock={true}
+            />
+        </div>
+    );
+};
 
 
 export default Deliver;
--- server/server.js
+++ server/server.js
@@ -1191,7 +1191,7 @@ app.post('/api/modifyDelivery', async (req, res) => {
         } else {
 
                 console.log(req.body.DeliveryDate);
-                let day = dayjs(req.body.DeliveryDate).format("DD-MM-YYYY HH:MM");
+                let day = dayjs(req.body.DeliveryDate).format("DD-MM-YYYY HH:mm");
                 console.log(day);
                 await db.collection('Order').doc(req.body.OrderID).update({DeliveryDate: day, DeliveryPlace: req.body.DeliveryPlace });
             
