e3dd7c84894d2c56481788c57d90f8e7cdbadfeb
bl4ckj4c <jacopo.marino.1998@gmail.com>
2022-01-02 18:43:37 +0100

    Update clone script

--- test/server-test/CloneFirebase.py
+++ test/server-test/CloneFirebase.py
@@ -7,19 +7,12 @@ import time
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-credBackup = credentials.Certificate(
-    "./polito-se2-21-01-spg-backup-firebase-adminsdk-toolm-860caeccd2.json"
-)
 credBackup2 = credentials.Certificate(
     "./polito-se2-21-01-spg-backup-2-firebase-adminsdk-rdqbs-c08f51d712.json"
 )
 credBackup3 = credentials.Certificate(
     "./polito-se2-21-01-spg-backup-3-firebase-adminsdk-kn5e2-979f8d4105.json"
 )
-
-appBackup = firebase_admin.initialize_app(credBackup, {
-    'projectId': 'polito-se2-21-01-spg-backup'
-}, "Backup")
 
 appBackup2 = firebase_admin.initialize_app(credBackup2, {
     'projectId': 'polito-se2-21-01-spg-backup-2'
@@ -29,49 +22,47 @@ appBackup3 = firebase_admin.initialize_app(credBackup3, {
     'projectId': 'polito-se2-21-01-spg-backup-3'
 }, "Backup3")
 
-
-dbBackup = firestore.client(appBackup)
 dbBackup2 = firestore.client(appBackup2)
 dbBackup3 = firestore.client(appBackup3)
 
 # Remove old data from backup2 and backup3
 def cleanBackup2And3():
     # User
-    docs = dbBackup2.collection(u'User').get()
-    for doc in docs:
-        dbBackup2.collection(u'User').document(doc.id).delete()
+    #docs = dbBackup2.collection(u'User').get()
+    #for doc in docs:
+        #dbBackup2.collection(u'User').document(doc.id).delete()
     docs = dbBackup3.collection(u'User').get()
     for doc in docs:
         dbBackup3.collection(u'User').document(doc.id).delete()
 
     # Farmer
-    docs = dbBackup2.collection(u'Farmer').get()
-    for doc in docs:
-        dbBackup2.collection(u'Farmer').document(doc.id).delete()
+    #docs = dbBackup2.collection(u'Farmer').get()
+    #for doc in docs:
+        #dbBackup2.collection(u'Farmer').document(doc.id).delete()
     docs = dbBackup3.collection(u'Farmer').get()
     for doc in docs:
         dbBackup3.collection(u'Farmer').document(doc.id).delete()
 
     # Product
-    docs = dbBackup2.collection(u'Product').get()
-    for doc in docs:
-        dbBackup2.collection(u'Product').document(doc.id).delete()
+    #docs = dbBackup2.collection(u'Product').get()
+    #for doc in docs:
+        #dbBackup2.collection(u'Product').document(doc.id).delete()
     docs = dbBackup3.collection(u'Product').get()
     for doc in docs:
         dbBackup3.collection(u'Product').document(doc.id).delete()
 
     # Order
-    docs = dbBackup2.collection(u'Order').get()
-    for doc in docs:
-        dbBackup2.collection(u'Order').document(doc.id).delete()
+    #docs = dbBackup2.collection(u'Order').get()
+    #for doc in docs:
+        #dbBackup2.collection(u'Order').document(doc.id).delete()
     docs = dbBackup3.collection(u'Order').get()
     for doc in docs:
         dbBackup3.collection(u'Order').document(doc.id).delete()
 
     # Product by Farmers
-    docs = dbBackup2.collection(u'Product by Farmers').get()
-    for doc in docs:
-        dbBackup2.collection(u'Product by Farmers').document(doc.id).delete()
+    #docs = dbBackup2.collection(u'Product by Farmers').get()
+    #for doc in docs:
+        #dbBackup2.collection(u'Product by Farmers').document(doc.id).delete()
     docs = dbBackup3.collection(u'Product by Farmers').get()
     for doc in docs:
         dbBackup3.collection(u'Product by Farmers').document(doc.id).delete()
@@ -88,37 +79,37 @@ def copyDataFromBackupTo2And3PlusLocal():
     }
 
     # User
-    docs = dbBackup.collection(u'User').get()
+    docs = dbBackup2.collection(u'User').get()
     for doc in docs:
-        dbBackup2.collection(u'User').document(doc.id).set(doc.to_dict())
+        #dbBackup2.collection(u'User').document(doc.id).set(doc.to_dict())
         dbBackup3.collection(u'User').document(doc.id).set(doc.to_dict())
         backup["users"][doc.id] = doc.to_dict()
 
     # Farmer
-    docs = dbBackup.collection(u'Farmer').get()
+    docs = dbBackup2.collection(u'Farmer').get()
     for doc in docs:
-        dbBackup2.collection(u'Farmer').document(doc.id).set(doc.to_dict())
+        #dbBackup2.collection(u'Farmer').document(doc.id).set(doc.to_dict())
         dbBackup3.collection(u'Farmer').document(doc.id).set(doc.to_dict())
         backup["farmers"][doc.id] = doc.to_dict()
 
     # Product
-    docs = dbBackup.collection(u'Product').get()
+    docs = dbBackup2.collection(u'Product').get()
     for doc in docs:
-        dbBackup2.collection(u'Product').document(doc.id).set(doc.to_dict())
+        #dbBackup2.collection(u'Product').document(doc.id).set(doc.to_dict())
         dbBackup3.collection(u'Product').document(doc.id).set(doc.to_dict())
         backup["products"][doc.id] = doc.to_dict()
 
     # Order
-    docs = dbBackup.collection(u'Order').get()
+    docs = dbBackup2.collection(u'Order').get()
     for doc in docs:
-        dbBackup2.collection(u'Order').document(doc.id).set(doc.to_dict())
+        #dbBackup2.collection(u'Order').document(doc.id).set(doc.to_dict())
         dbBackup3.collection(u'Order').document(doc.id).set(doc.to_dict())
         backup["orders"][doc.id] = doc.to_dict()
 
     # Product by Farmers
-    docs = dbBackup.collection(u'Product by Farmers').get()
+    docs = dbBackup2.collection(u'Product by Farmers').get()
     for doc in docs:
-        dbBackup2.collection(u'Product by Farmers').document(doc.id).set(doc.to_dict())
+        #dbBackup2.collection(u'Product by Farmers').document(doc.id).set(doc.to_dict())
         dbBackup3.collection(u'Product by Farmers').document(doc.id).set(doc.to_dict())
         backup["productsByFarmers"][doc.id] = doc.to_dict()
 
@@ -137,9 +128,7 @@ def makeLocalCopyOfFirebase(sourceDB=1):
         "productsByFarmers": {}
     }
     
-    if(sourceDB == 1):
-        db = dbBackup
-    elif(sourceDB == 2):
+    if(sourceDB == 2):
         db = dbBackup2
     elif(sourceDB == 3):
         db = dbBackup3
@@ -183,6 +172,13 @@ def makeLocalCopyOfFirebase(sourceDB=1):
     f = open("./backup_" + str(timestamp) + ".json", "w")
     f.write(json.dumps(backup, indent=4))
 
+def updateWeek(week=1):
+    # Product by Farmers
+    docs = dbBackup3.collection(u'Product by Farmers').get()
+    for doc in docs:
+        dbBackup3.collection(u'Product by Farmers').document(doc.id).update({u'Week': week})
+
+
 # Read data from backup firebase,
 # store them in bakcup2 and bakup3,
 # plus make a local copy in JSON format
@@ -190,7 +186,9 @@ if __name__ == '__main__':
     #cleanBackup2And3()
     #copyDataFromBackupTo2And3PlusLocal()
 
-    makeLocalCopyOfFirebase(sourceDB=3)
+    updateWeek()
+
+    #makeLocalCopyOfFirebase(sourceDB=2)
 
     print('DONE')
 
