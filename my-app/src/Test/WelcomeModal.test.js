5474151865708a3c77dd2abcead313d3e5f13022
bl4ckj4c <jacopo.marino.1998@gmail.com>
2022-01-04 17:11:39 +0100

    Fix for tests

--- my-app/src/Test/WelcomeModal.test.js
+++ my-app/src/Test/WelcomeModal.test.js
@@ -3,16 +3,63 @@ import {render, fireEvent, waitFor} from '@testing-library/react';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import WelcomeModal from "../Components/WelcomeModal";
-//import WelcomeModal from './WelcomeModal'
 
 import API from "../API";
+import Welcome from "../Components/Welcome";
 
 describe('Test for WelcomeModal.js', () => {
+
+    const mockSetWelcomeShow = jest.fn();
+
     test('Correct render of the component', async () => {
         const {getByText} = render(
             <Router>
-                <WelcomeModal/>
+                <WelcomeModal
+                    show={true}
+                    onHide={mockSetWelcomeShow}/>
             </Router>
         );
+    });
+
+    test('Login', async () => {
+        const {getByText, getByLabelText, getByPlaceholderText} = render(
+            <Router>
+                <WelcomeModal
+                    show={true}
+                    onHide={mockSetWelcomeShow}/>
+            </Router>
+        );
+
+        await waitFor(() => {
+            fireEvent.click(getByText('Login'));
+        });
+    });
+
+    test('Sign Up', async () => {
+        const {getByText, getByLabelText, getByPlaceholderText} = render(
+            <Router>
+                <WelcomeModal
+                    show={true}
+                    onHide={mockSetWelcomeShow}/>
+            </Router>
+        );
+
+        await waitFor(() => {
+            fireEvent.click(getByText('Sign Up'));
+        });
+    });
+
+    test('Take a quick look', async () => {
+        const {getByText, getByLabelText, getByPlaceholderText} = render(
+            <Router>
+                <WelcomeModal
+                    show={true}
+                    onHide={mockSetWelcomeShow}/>
+            </Router>
+        );
+
+        await waitFor(() => {
+            fireEvent.click(getByText('or take a quick look'));
+        });
     });
 });
